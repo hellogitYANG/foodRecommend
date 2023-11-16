2eaa520f40c468d2dc808d5e1b1cba9ac2ccc131
YanWeiyi <2764548316@qq.com>
2023-11-17 00:05:54 +0800

    完善文件上传接口/举报信息接口，将文件上传调整为七牛云服务器。

---
+++ src/main/java/com/example/foodrecommend/utils/QiniuUtils.java
@@ -0,0 +1,83 @@
+package com.example.foodrecommend.utils;
+
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.BucketManager;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
+
+/**
+ * 七牛云工具类
+ */
+public class QiniuUtils {
+    public static String accessKey = "QyukWedANdUzQrJW73S-m7g5Um6qn2-Os5Bd_6zq";
+    public static String secretKey = "k_E4RwXjQXmRpB82gPmEhF2sAP18DDohaJWnOdfF";
+    public static String bucket = "itmcat-space";
+
+    public static void upload2Qiniu(String filePath, String fileName) {
+        // 构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone0());
+        UploadManager uploadManager = new UploadManager(cfg);
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        try {
+            Response response = uploadManager.put(filePath, fileName, upToken);
+            // 解析上传成功的结果
+            DefaultPutRet putRet =
+                    new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            try {
+                System.err.println(r.bodyString());
+            } catch (QiniuException ex2) {
+                // ignore
+            }
+        }
+    }
+
+    // 上传文件
+    public static void upload2Qiniu(byte[] bytes, String fileName) {
+        // 构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone0());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        // 默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = fileName;
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        try {
+            Response response = uploadManager.put(bytes, key, upToken);
+            // 解析上传成功的结果
+            DefaultPutRet putRet =
+                    new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            System.err.println(r.toString());
+            try {
+                System.err.println(r.bodyString());
+            } catch (QiniuException ex2) {
+                // ignore
+            }
+        }
+    }
+
+    // 删除文件
+    public static void deleteFileFromQiniu(String fileName) {
+        // 构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone0());
+        String key = fileName;
+        Auth auth = Auth.create(accessKey, secretKey);
+        BucketManager bucketManager = new BucketManager(auth, cfg);
+        try {
+            bucketManager.delete(bucket, key);
+        } catch (QiniuException ex) {
+            // 如果遇到异常，说明删除失败
+            System.err.println(ex.code());
+            System.err.println(ex.response.toString());
+        }
+    }
+}
